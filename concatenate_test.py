8b73a6f69e2a8e5a032d454859824ed716eb2161
515hikaru <12kojima.takahiro@gmail.com>
2020-05-02 14:08:42 +0900

    add tests
    for resolve #1

--- concatenate_test.py
+++ concatenate_test.py
@@ -2,5 +2,27 @@
 testing
 """
 
+import pytest
+
+from concatenate import concat, insert_newpage, preprocess, trim_end_newpage
+
+
 def test_concat_string():
-    assert 1 == 1
+    base = ""
+    strings = ["foo\n", "bar\n"]
+    expected = "foo\nbar\n"
+    actual = concat(base, strings)
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [(["foo\n"], ["　foo\n"]), (["bar\n", "boo\n", "\n", "boo\n"], ["　bar\n", "　boo\n", "\n", "　boo\n"])]
+)
+def test_preprocess(test_input, expected):
+    results = preprocess(test_input)
+    for result, exp in zip(results, expected):
+        assert result == exp
+
+def test_insert_newpage():
+    
